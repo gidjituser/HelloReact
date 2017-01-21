9f37802eccb729d0b58002262dfe0a83c2909427
Chris Page <cp@concerco.com>
2017-01-21 13:38:43 -0500

    added initial files

---
+++ views/ios/index.ios.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class WhyReactNativeIsSoGreat extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Example Gidjit Launch view
+        </Text>
+        <Text>
+          This just uses components 'View' and 'Text',
+          The functionality/features of React native with Gidjit will improve 
+          dramatically over the next few months.
+        </Text>
+      </View>
+    );
+  }
+}
+
