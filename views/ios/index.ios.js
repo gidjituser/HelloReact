581da42817687d44ff76ce0e00b47594cf51c6d8
Chris Page <cp@concerco.com>
2017-02-03 17:16:37 -0500

    dev true, remote image

--- views/ios/index.ios.js
+++ views/ios/index.ios.js
@@ -18,25 +18,25 @@ class GidjitBasicExample extends Component {
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-        <Image  style={{width: 60, height: 60}} source={require('../../icon.png')} />
+        <Image  style={{width: 60, height: 60}} source={{ uri: 'http://is2.mzstatic.com/image/thumb/Purple91/v4/83/24/37/8324375c-eaa1-3306-ca66-d3a849133888/source/175x175bb.jpg' }} />
         <Text style={{width: 250, height: 20}}>
           Very basic Gidjit React Native view
         </Text>
-        <Text style={{width: 250, height: 50}}>
+        <Text style={{width: 250, height: 150}}>
           This example just uses components 'View' and 'Text', however most if not all React Native elements can
-          be used. You can find the source allowing with all the instructions to create and add your own
+          be used. You can find the source along with all the instructions to create and add your own React Native action
           <Text style={{color: 'blue'}}
             onPress={() => Linking.openURL('https://github.com/gidjituser/HelloReact')}
-            style={{width: 50, height: 50}}>
+            style={{width: 150, height: 50}}>
             here.
           </Text>
         </Text>
-        <Text style={{width: 250, height: 50}}>
+        <Text style={{width: 250, height: 150}}>
           This example just uses components 'View' and 'Text', however most if not all React Native elements can
           be used. You can find an example where an existing project was forked and bundled so
           that it would work with Gidjit. It was not written by ourselves.
         </Text>
-        <Text style={{color: 'blue'}}
+        <Text style={{color: 'blue'}}>
           onPress={() => Linking.openURL('https://github.com/gidjituser/FinanceReactNative')}
           style={{width: 50, height: 50}}>
           FinanceReactNative
