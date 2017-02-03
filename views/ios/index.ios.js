3dd8cc198e2eb7babd2704f42c34cad689ba46ce
Chris Page <cp@concerco.com>
2017-02-03 17:02:05 -0500

    updated view and icon

--- views/ios/index.ios.js
+++ views/ios/index.ios.js
@@ -7,22 +7,44 @@ import {
 } from 'react-native';
 
 
-class WhyReactNativeIsSoGreat extends Component {
+class GidjitBasicExample extends Component {
   render() {
     return (
-      <View>
-        <Text>
-          Example Gidjit Launch view
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <Image  style={{width: 60, height: 60}} source={require('../../icon.png')} />
+        <Text style={{width: 250, height: 20}}>
+          Very basic Gidjit React Native view
         </Text>
-        <Text>
-          This just uses components 'View' and 'Text',
-          The functionality/features of React native with Gidjit will improve 
+        <Text style={{width: 250, height: 50}}>
+          This example just uses components 'View' and 'Text', however most if not all React Native elements can
+          be used. You can find the source allowing with all the instructions to create and add your own
+          <Text style={{color: 'blue'}}
+            onPress={() => Linking.openURL('https://github.com/gidjituser/HelloReact')}
+            style={{width: 50, height: 50}}>
+            here.
+          </Text>
+        </Text>
+        <Text style={{width: 250, height: 50}}>
+          This example just uses components 'View' and 'Text', however most if not all React Native elements can
+          be used. You can find an example where an existing project was forked and bundled so
+          that it would work with Gidjit. It was not written by ourselves.
+        </Text>
+        <Text style={{color: 'blue'}}
+          onPress={() => Linking.openURL('https://github.com/gidjituser/FinanceReactNative')}
+          style={{width: 50, height: 50}}>
+          FinanceReactNative
+        </Text>
+        <Text style={{width: 250, height: 50}}>
+          The functionality/features of React Native with Gidjit will improve
           dramatically over the next few months.
         </Text>
       </View>
     );
   }
 }
-AppRegistry.registerComponent('WhyReactNativeIsSoGreat', () => WhyReactNativeIsSoGreat);
-
-
+AppRegistry.registerComponent('GidjitBasicExample', () => GidjitBasicExample);
