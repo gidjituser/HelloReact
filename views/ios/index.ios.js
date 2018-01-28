7585f4d070c9498338863452f6c7721a83811666
Chris Page <cp@concerco.com>
2018-01-28 13:12:09 -0500

    fixed image link

--- views/ios/index.ios.js
+++ views/ios/index.ios.js
@@ -1,3 +1,4 @@
+// @flow
 import React, {Component} from 'react';
 import {
   AppRegistry,
@@ -5,13 +6,14 @@ import {
   Text,
   Image,
   Linking,
+  TouchableOpacity,
   View
 } from 'react-native';
 
 import { rnImage } from './resources'
 
 
-class GidjitBasicExample extends Component {
+export class GidjitBasicExample extends Component {
   render() {
     return (
       <View style={{
@@ -20,32 +22,23 @@ class GidjitBasicExample extends Component {
         justifyContent: 'space-around',
         alignItems: 'center',
       }}>
-      {/*
-        <Image  style={{width: 60, height: 60}} source={require('../../img/appIcon.png')} />
-        */}
-        <Image style={{height: 100}} source={{uri: rnImage}} />
+        <Image style={{width: 100, height: 100}} source={{uri: rnImage}} />
 
         <Text style={[styles.bigblack, styles.seperateInfo]}>
           Very basic Gidjit React Native view
         </Text>
-        <Text style={[styles.textInfo, {width: 250}]}>
-          This example just uses components 'View' and 'Text', however most if not all React Native elements can
-          be used. You can find the source along with all the instructions to create, add, and easily share your own React Native App   
-          <Text style={styles.bigblue}
-            onPress={() => Linking.openURL('https://github.com/gidjituser/HelloReact')} >
-             here.
+        <View style={{flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={[styles.textInfo, {width: 250}]}>
+            This example just uses components 'View' and 'Text', however most if not all React Native elements can
+            be used. You can find the source along with all the instructions to create, add, and easily share your own React Native App
+            by clicking the link below.
           </Text>
-        </Text>
-        {/*
-        <Text style={{width: 250}}>
-          You can find an example where an existing RN project was forked from github and bundled so
-          that it would work with Gidjit (It was not written by ourselves).
-        </Text>
-        <Text style={[styles.bigblue, styles.seperateInfo]}
-          onPress={() => Linking.openURL('https://github.com/gidjituser/FinanceReactNative')}>
-          FinanceReactNative
-        </Text>
-        */}
+            <TouchableOpacity style={[{width: 130, height: 24, alignSelf:'center'}]} sonPress={() => Linking.openURL('https://github.com/gidjituser/HelloReact')}>
+              <Text style={styles.bigblue}>
+                HelloReact
+              </Text>
+            </TouchableOpacity>
+        </View>
         <Text style={[styles.textInfo, {width: 250}]}>
           The functionality/features of React Native with Gidjit will improve during each release.
         </Text>
